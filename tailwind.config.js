aef5cfae26ae479ac7e0f000913930085ad9849b
jamie <jamiekaram16@gmail.com>
2021-09-06 14:57:24 -0400

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './sections/**/ *.{ js, ts, jsx, tsx }'],
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './sections/**/ *.{ js, ts, jsx, tsx }'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
@@ -23,12 +23,7 @@ module.exports = {
       yellow: colors.amber,
 
     },
-    extend: {
-      backgroundImage: theme => ({
-        'example': "url('https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80')",
 
-      })
-    },
   },
   variants: {
     extend: {},
