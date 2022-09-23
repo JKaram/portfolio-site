8f8517b6c71de080cde1af6baf95c63320c87dd9
Jamie Karam <jamiekaram16@gmail.com>
2022-09-22 22:46:24 -0400

    fix typo 'oppertunities'

--- sections/Hero/Hero.tsx
+++ sections/Hero/Hero.tsx
@@ -7,14 +7,14 @@ export default function Hero() {
     <div className="p-4">
       <h1 className="my-8 text-4xl font-bold font-nunito">Hi, I&apos;m Jamie 👋</h1>
       <p className="text-2xl md:leading-10 md:text-3xl">
-        I&apos;m currently looking for new oppertunities. Located in Ottawa, Canada 🇨🇦. Thank you for checking out my
-        site. I post small projects and anything I find interesting.
+        I&apos;m currently looking for new opportunities. Located in Ottawa, Canada. Thank you for checking out my site.
+        This is where I post side projects.
       </p>
       <p className="mt-4 text-2xl md:text-3xl md:leading-10">
         I focus on front-end development. I love <Span color={badgeColours["JavaScript"]}>JavaScript</Span>,&nbsp;
         <Span color={badgeColours["TypeScript"]}>TypeScript</Span>, and <Span color="#61DAFB">React</Span>. I am also
-        interested in Data Visualization and Data Science. In my past life, I worked in Television as a Camera Operator.
-        In my spare time, I work on side projects, exercise and photography 📷.
+        interested in Data Visualization and Data Science. In my past life, I worked in television as a camera operator.
+        In my spare time I work on projects, run, swim and photography.
       </p>
       <p className="mt-4 text-2xl md:text-3xl md:leading-10">
         Please reach out me on{" "}
