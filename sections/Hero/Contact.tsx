3aa78d34c025c64f2761fcac4ca48f56dcd56ab8
Jamie Karam <jamiekaram16@gmail.com>
2022-09-28 19:50:11 -0400

    add label to contact icons

--- sections/Hero/Contact.tsx
+++ sections/Hero/Contact.tsx
@@ -1,16 +1,24 @@
+import { useTranslation } from "next-i18next";
 import React from "react";
 import Icon from "./Icon";
 
 function Contact({ className }) {
+  const { t } = useTranslation();
   return (
     <div className={className}>
       <Icon
         icon="resume"
         link="https://www.dropbox.com/s/vkkblcxjhkn7x5g/Jamie_Karam_Resume.pdf?dl=0"
         title="Link to my resume"
+        label={t("contact.resume")}
       />
-      <Icon icon="gitHub" link="https://github.com/JKaram" title="Link to my GitHub" />
-      <Icon icon="linkedIn" link="https://www.linkedin.com/in/jamiekaram/" title="Link to my Linkedin" />
+      <Icon icon="gitHub" link="https://github.com/JKaram" title="Link to my GitHub" label="GitHub" />
+      <Icon
+        icon="linkedIn"
+        label="LinkedIn"
+        link="https://www.linkedin.com/in/jamiekaram/"
+        title="Link to my Linkedin"
+      />
     </div>
   );
 }
--- sections/Hero/Icon.tsx
+++ sections/Hero/Icon.tsx
@@ -7,17 +7,25 @@ type Icon = typeof icons[number];
 
 type Props = {
   icon: Icon;
+  label: string;
   link: string;
   title: string;
 };
 
 export default function Icon(props: Props) {
-  const { icon, link, title } = props;
+  const { icon, link, title, label } = props;
   return (
-    <a href={link} target="_blank" rel="noreferrer" className="opacity-50 hover:opacity-100" title={title}>
-      {icon === "gitHub" && <AiFillGithub />}
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="flex flex-col items-center opacity-75 hover:opacity-100"
+      title={title}
+    >
+      {icon === "gitHub" && <AiFillGithub className="" />}
       {icon === "linkedIn" && <AiFillLinkedin className="hover:text-linkedIn" />}
       {icon === "resume" && <BsFillFileEarmarkPersonFill />}
+      <span className="p-2 text-sm font-nunito">{label}</span>
     </a>
   );
 }
