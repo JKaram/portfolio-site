eac19b61485629a41d1ca0f3c48b9db28315cfcf
Jamie Karam <jamiekaram16@gmail.com>
2022-10-07 12:12:59 -0400

    remove flags from language selector

--- sections/Nav/LanguageSelector.tsx
+++ sections/Nav/LanguageSelector.tsx
@@ -1,14 +1,11 @@
 import React, { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import classNames from "classnames";
-import canada from "../../public/logos/ca.svg";
-import france from "../../public/logos/fr.svg";
 import { useRouter } from "next/router";
-import Image from "next/image";
-
+import { MdLanguage } from "react-icons/md";
 const languages = [
-  { id: 1, name: "English", code: "en", img: canada },
-  { id: 2, name: "French", code: "fr", img: france },
+  { id: 1, name: "English", code: "en" },
+  { id: 2, name: "French", code: "fr" },
 ];
 
 function LanguageSelector() {
@@ -26,11 +23,11 @@ function LanguageSelector() {
     <Listbox value={selected} onChange={changeLanguage}>
       {({ open }) => (
         <>
-          <div className="relative hidden m-auto text-xl md:block dark:text-white">
+          <div className="relative hidden m-auto md:block dark:text-white min-wdth-[95px]">
             <Listbox.Button className="relative w-full px-3 py-2 text-left rounded-md cursor-default focus:border-red-100 focus:outline-none focus:ring-1 focus:ring-indigo-900 dark:focus:ring-red-100 sm:text-sm">
-              <span className="flex items-center w-24">
-                <Image src={selected.img} layout="fixed" alt="flag" width={25} height={20} />
-                <span className="block ml-3 ">{selected.name}</span>
+              <span className="flex items-center ">
+                <MdLanguage />
+                <span className="block ml-2 text-base ">{selected.name}</span>
               </span>
             </Listbox.Button>
 
@@ -41,7 +38,7 @@ function LanguageSelector() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-x-hidden text-base rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {languages
                   .filter((language) => language.name !== selected.name)
                   .map((language) => (
@@ -49,15 +46,14 @@ function LanguageSelector() {
                       key={language.id}
                       className={({ active }) =>
                         classNames(
-                          active ? "bg-gray-200 dark:bg-gray-500" : undefined,
-                          "relative cursor-default select-none py-2 pl-3 "
+                          active ? "bg-gray-200 dark:bg-gray-500 " : undefined,
+                          "relative cursor-default select-none py-2 pl-3 text-base"
                         )
                       }
                       value={language}
                     >
                       {({ selected }) => (
                         <span className={classNames("flex items-center w-24")}>
-                          <Image src={language.img} layout="fixed" alt="flag" width={25} height={20} />
                           <span
                             className={classNames(selected ? "font-semibold" : "font-normal", "block truncate ml-3")}
                           >
